640dfb0843babdbb7f623ac18e316f6b108e6e95
marmo002 <marmo_02@hotmail.com>
2017-11-17 13:04:39 -0500

    finish first part of assignment

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener("DOMContentLoaded", function() {
+
+  var tweetForm = document.getElementById('new_tweet');
+
+  console.log(tweetForm);
+  tweetForm.addEventListener('submit', function(e){
+
+    e.preventDefault();
+
+    $.ajax({
+      url: tweetForm.getAttribute('action'),
+      method: tweetForm.getAttribute('method'),
+      dataType: 'HTML',
+      data: $(tweetForm).serialize()
+    }).done(function(tweet){
+      console.log(tweet);
+      var ul = document.querySelector('.tweets');
+      ul.insertAdjacentHTML('afterbegin', tweet);
+    });
+  });
+
+});
